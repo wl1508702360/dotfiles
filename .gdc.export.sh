3f44a75f537e9af05c7d164110c2a0e72698ae79
gdc <dickens.gao@gmail.com>
2017-01-15 01:00:56 +0800

    some more export

---
+++ .gdc.export.sh
@@ -0,0 +1,25 @@
+# for visudo or other
+export EDITOR=vim
+
+export TERM=xterm-256color
+export SOCKS5_SERVER=127.0.0.1:1080
+
+# for php dev
+export www=/var/www/html
+export php=/var/www/html/php
+export api=/var/www/html/php/api
+export api2=/var/www/html/php/api2/dist
+export controller=$api2/app/Http/Controllers/v1
+export routes=$api2/routes
+export bootstrap=$api2/bootstrap
+export vendor=$api2/vendor
+export exception=$api2/app/Exceptions
+
+export NODE_PATH=/usr/local/lib/node_modules
+
+# Rhythmbox encoding
+export GST_ID3_TAG_ENCODING=GBK:UTF-8:GB18030
+export GST_ID3V2_TAG_ENCOD ING=GBK:UTF-8:GB18030
+
+# man
+export MANWIDTH=80
--- .gdc.sh
+++ .gdc.sh
@@ -7,30 +7,19 @@
 # And add below line to $HOME/.bashrc file to get itself included.
 # code : . $HOME/.gdc.sh
 
-# export var
-export SOCKS5_SERVER=127.0.0.1:1080
-export EDITOR=vim
-export sdccinclude=/usr/share/sdcc/include
-export MINICOM='-m -c on'
-export www=/var/www/html
-export php=/var/www/html/php
-export api=/var/www/html/php/api
-export api2=/var/www/html/php/api2/dist
-export controller=$api2/app/Http/Controllers/v1
-export routes=$api2/routes
-export bootstrap=$api2/bootstrap
-export vendor=$api2/vendor
-export exception=$api2/app/Exceptions
-export TERM=xterm-256color
-export NODE_PATH=/usr/local/lib/node_modules
+# gdc's alias
+if [[ -f ~/.gdc.alias.sh ]]; then
+  source ~/.gdc.alias.sh
+fi
+
+# gdc's export
+if [[ -f ~/.gdc.export.sh ]]; then
+  source ~/.gdc.export.sh
+fi
+
 if [ -d $HOME/.config/composer/vendor/bin ]; then
     export PATH=$HOME/.config/composer/vendor/bin:$PATH
 fi
-#Rhythmbox encoding
-export GST_ID3_TAG_ENCODING=GBK:UTF-8:GB18030
-export GST_ID3V2_TAG_ENCOD ING=GBK:UTF-8:GB18030
-# man
-export MANWIDTH=80
 
 # load ~/.Xdefaults
 [[ -e $HOME/.Xdefaults ]] && [[ -n ${DISPLAY} ]] && xrdb -merge $HOME/.Xdefaults  > /dev/null 2>&1
