685d0f3516187bacedbf8eab9d41ce964e782cae
Dacheng Gao <realgaodacheng@gmail.com>
2017-09-24 21:33:42 +0800

    proxy.sh

--- bin/proxy.sh
+++ bin/proxy.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 #
 
-#sudo sslocal -c /etc/shadowsocks/sgp01.ggddcc.com.json -d restart 2>/dev/null
 #sudo sslocal -c /etc/shadowsocks/us01.ggddcc.com.json -d restart 2>/dev/null
-sudo sslocal -c /etc/shadowsocks/hk01.ggddcc.com.json -d restart 2>/dev/null
+sudo sslocal -c /etc/shadowsocks/us02.ggddcc.com.json -d restart 2>/dev/null
